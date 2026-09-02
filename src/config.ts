7f765f51bf34627035f21d95179aa4c0a55b5c1f
AslamGeek <aslamzblogger@gmail.com>
2026-09-02 23:09:19 +0530

    Update Apps Script deployment URL

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SPREADSHEET_ID =
 
 export const GAS_WEB_APP_URL =
   import.meta.env.VITE_GAS_WEB_APP_URL ||
-  'https://script.google.com/macros/s/AKfycbzQItrxiYszeoHIoXC07uhnmiYanvVRwvSoz3QdMuWYU-9jUaoo8mL6Bg2mG3bpwNb8/exec'
+  'https://script.google.com/macros/s/AKfycbzlzn1Q9C2lppXa7E8Zo1UH4QfWAvXf6ufqP0hPw7Vmvdb_hr5RduxT5iLQVIfKI4R3/exec'
 
 export const API_TIMEOUT_MS = 20_000
 export const UNDO_WINDOW_MS = 8_000
